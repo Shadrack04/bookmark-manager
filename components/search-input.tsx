c4030fec091065abade1b9137fd40486b99e1876
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-10-17 13:43:42 +0100

    create and style the mobile view of the top bar

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -10,18 +10,13 @@ export default function SearchInput() {
       <div className="relative">
         <Input
           id={id}
-          className="peer ps-9 pe-9"
-          placeholder="Search..."
+          className="peer ps-9 pe-9 h-12 w-46 placeholder:text-xs"
+          placeholder="Search by title..."
           type="search"
         />
         <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50">
           <SearchIcon size={16} />
         </div>
-        <button
-          className="absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md text-muted-foreground/80 transition-[color,box-shadow] outline-none hover:text-foreground focus:z-10 focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
-          aria-label="Submit search"
-          type="submit"
-        ></button>
       </div>
     </div>
   );
--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -7,7 +7,7 @@ import ProfileDisplay from "./profile-display";
 export default function TopBar() {
   return (
     <nav className=" custom-padding flex items-center justify-between w-full h-20 bg-foreground">
-      <Menu className=" text-secondary p-2 border-2 size-12 rounded-lg cursor-pointer" />
+      <Menu className=" text-secondary p-2 border-border border-2 size-12 rounded-lg cursor-pointer" />
       <SearchInput />
       <Button className=" px-2 py-3 h-auto">
         <Plus className=" size-6" /> <span className=" hidden"></span>
