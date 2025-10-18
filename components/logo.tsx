11b1b0df84e74aeb5d2d0f6a3cc8df3f5dba1545
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-10-18 05:21:47 +0100

    style the side bar navigation

---
+++ components/logo.tsx
@@ -0,0 +1,18 @@
+import { Bookmark } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type LogoProps = {
+  href?: string;
+};
+
+export default function Logo({ href }: LogoProps) {
+  return (
+    <Link href={href || "/"} className=" flex gap-2">
+      <Bookmark className=" size-8 p-1 bg-primary rounded-sm" />
+      <span className=" text-lg text-secondary font-bold">
+        Bookmark Manager
+      </span>
+    </Link>
+  );
+}
--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import Logo from "./logo";
+import { Archive, Home, LucideIcon } from "lucide-react";
+import Link from "next/link";
+
+type NavLinks = {
+  href: string;
+  name: string;
+  icon: LucideIcon;
+};
+
+const navLinks: NavLinks[] = [
+  {
+    icon: Home,
+    name: "Home",
+    href: "/",
+  },
+  {
+    icon: Archive,
+    name: "Archived",
+    href: "/archived",
+  },
+];
 
 export default function SideBar() {
   return (
-    <div className=" hidden lg:flex w-82 border-1 bg-foreground min-h-screen">
-      side-bar
+    <div className=" hidden lg:flex flex-col gap-4 w-82 border-1 bg-foreground min-h-screen px-4">
+      <div className="h-20 lg:h-18 flex items-center">
+        <Logo />
+      </div>
+      <div className=" flex flex-col gap-1">
+        {navLinks?.map(({ name, href, icon: Icon }) => (
+          <Link
+            key={name}
+            href={href}
+            className=" text-muted hover:text-secondary hover:bg-background/80 py-2 px-2 flex rounded-md items-center gap-2"
+          >
+            <Icon />
+            <span>{name}</span>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
