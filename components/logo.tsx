014b4275fd931c3eea06e3c6164e9c8d4739ca5a
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-10-19 11:13:52 +0100

    create the sign up form

--- components/logo.tsx
+++ components/logo.tsx
@@ -9,7 +9,7 @@ type LogoProps = {
 export default function Logo({ href }: LogoProps) {
   return (
     <Link href={href || "/"} className=" flex gap-2">
-      <Bookmark className=" size-8 p-1 bg-primary rounded-sm" />
+      <Bookmark className=" size-8 p-1 bg-primary text-white rounded-sm" />
       <span className=" text-lg text-secondary font-bold">
         Bookmark Manager
       </span>
--- features/auth/components/signup-form.tsx
+++ features/auth/components/signup-form.tsx
@@ -1,62 +1,83 @@
+"use client";
+
+import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 export function SignupForm() {
+  const form = useForm();
   return (
-    <Card className="w-full max-w-sm">
+    <Card className=" w-[95%] mx-auto">
       <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
+        <div className=" mb-4">
+          <Logo />
+        </div>
+        <CardTitle className=" text-2xl">Create your account</CardTitle>
+        <CardDescription className=" text-md">
+          Join us and start saving your favorite links-organized, searchable and
+          always within reach
         </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
       </CardHeader>
       <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
+        <Form {...form}>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Full Name</Label>
+                <Input
+                  id="fullName"
+                  type="text"
+                  placeholder="Enter full name"
+                  required
+                />
               </div>
-              <Input id="password" type="password" required />
+
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
             </div>
-          </div>
-        </form>
+            <div className=" flex ">
+              <Button type="submit" className="w-full">
+                Create account
+              </Button>
+            </div>
+          </form>
+        </Form>
       </CardContent>
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
+        <p>
+          Already have an account? <Link href="/sign-in">Log in</Link>
+        </p>
       </CardFooter>
     </Card>
   );
