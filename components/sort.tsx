2b41d3c721d4749dd789dffc081eeda60a953933
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-10-20 22:57:43 +0100

    add hover state to sort by button

--- components/sort.tsx
+++ components/sort.tsx
@@ -4,7 +4,7 @@ import { Card } from "./ui/card";
 
 export default function Sort() {
   return (
-    <Card className=" p-3 bg-foreground flex-row items-center gap-1 rounded-md border-2 border-border cursor-pointer shadow-sm">
+    <Card className=" p-3 bg-foreground hover:bg-transparent flex-row items-center gap-1 rounded-md border-1 border-secondary/50 cursor-pointer shadow-sm">
       <ArrowDownUp className=" text-secondary" />
       <p className=" text-secondary text-md font-medium">Sort by</p>
     </Card>
