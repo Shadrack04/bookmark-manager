28182c901893601b45cc5692b75d0235f88fe54f
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-10-22 14:39:37 +0100

    create a session store with zustand

---
+++ store/sesstion.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface SessionState {
+  token: string | null;
+  user: any;
+  setSession: (token: string, user: any) => void;
+  clearSession: () => void;
+}
+
+export const useSessionStore = create<SessionState>()(
+  persist(
+    (set) => ({
+      token: null,
+      user: null,
+      setSession: (token, user) => set({ token, user }),
+      clearSession: () => set({ token: null, user: null }),
+    }),
+    { name: "session-storage" }
+  )
+);
