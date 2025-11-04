cffb77b786354d36fbb343a4c076d5b09510af6f
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-11-04 15:03:38 +0100

    Create const file containing url endpoint

---
+++ constants/endpoint.ts
@@ -0,0 +1,7 @@
+export const SIGN_UP_ENDPOINT = "/auth/sign-up";
+
+export const SIGN_IN_ENDPOINT = "/auth/sign-in";
+
+export const GET_ALL_BOOKMARKS_ENDPOINT = "/bookmark";
+
+export const GET_BOOKMARK_ENDPOINT = (id: string) => `/bookmark/${id}`;
