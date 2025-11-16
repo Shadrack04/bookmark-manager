cf0f725c8800c3bf0c38a50b680280e1d279b40a
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-11-16 10:04:38 +0100

    style the tag checkbox

--- components/tag-item.tsx
+++ components/tag-item.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import React from "react";
 import { Input } from "./ui/input";
+
+import { Label } from "./ui/label";
+import { Checkbox } from "./ui/checkbox";
 
 type TagItemProps = {
   isChecked: boolean;
@@ -15,8 +20,13 @@ export default function TagItem({
   return (
     <div className=" flex items-center justify-between">
       <div className=" flex items-center gap-2">
-        <Input type="checkbox" className=" size-4 rounded-sm" />
-        <p className=" text-muted">Ai</p>
+        <Checkbox
+          className=" size-5 data-[state=checked]:text-white"
+          id="terms"
+        />
+        <Label htmlFor="terms" className=" text-muted">
+          Accept terms
+        </Label>
       </div>
       <div className=" size-6 flex items-center justify-center rounded-full bg-background">
         <p className=" text-muted ">2</p>
