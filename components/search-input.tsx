1e81e4e3cfa05d5ef1a9ed767114ff9cd7290496
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-11-16 08:23:50 +0100

    implement the debounce and hook it with the search input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,19 +1,26 @@
-import { useId } from "react";
+import { useEffect, useId, useState } from "react";
 import { SearchIcon } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 import { useBookmarkFilterStore } from "@/store/bookmark-filter-store";
+import { useDebounce } from "@/hooks/use-debounce";
 
 export default function SearchInput() {
   const id = useId();
   const { search, setSearch } = useBookmarkFilterStore();
+  const [inputValue, setInputValue] = useState(search);
+  const debouncedValue = useDebounce(inputValue, 400);
+
+  useEffect(() => {
+    setSearch(debouncedValue);
+  }, [debouncedValue, setSearch]);
 
   return (
     <div className="*:not-first:mt-2">
       <div className="relative">
         <Input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           id={id}
           className="peer ps-9 pe-9 h-12 w-40 md:w-90 placeholder:text-[10px] md:placeholder:text-lg text-secondary  hover:bg-background border-2 border-primary"
           placeholder="Search by title..."
