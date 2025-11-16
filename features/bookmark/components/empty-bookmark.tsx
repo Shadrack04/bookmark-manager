35ffd1f9d5fd864a46bcb2af74f29996a8c52157
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-11-16 10:43:28 +0100

    fix

--- features/bookmark/components/empty-bookmark.tsx
+++ features/bookmark/components/empty-bookmark.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function EmptyBookmark() {
   return (
-    <div className=" flex flex-col items-center justify-center my-10">
+    <div className=" flex flex-col gap-3 items-center justify-center my-10">
       <p className=" text-secondary text-2xl font-bold">Empty Bookmark</p>
       <p className=" text-md text-muted">
         Click Add bookmark button to add your first bookmark
